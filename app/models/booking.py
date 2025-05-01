b13192eebc2c4023e75224695527d564eb3bd1b9
LATEAM99 <egonzl92@outlook.com>
2025-04-30 18:48:25 -0700

    booking model

---
+++ app/models/booking.py
@@ -0,0 +1,17 @@
+from typing import Optional
+from pydantic import BaseModel, Field
+from datetime import datetime
+from bson import ObjectId
+
+class Booking(BaseModel):
+    id: Optional[str] = Field(default_factory=lambda: str(ObjectId()))
+    product_id: str
+    start_date: datetime
+    end_date: datetime
+    status: str = "pending"
+
+    class Config:
+        allow_population_by_field_name = True
+        arbitrary_types_allowed = True
+        json_encoders = {ObjectId: str}
+        
